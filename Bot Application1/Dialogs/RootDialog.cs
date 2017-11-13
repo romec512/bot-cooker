8e50b314bc800800241d0402add7f6d1ae4c96b6
romec512 <roma-asadov@mail.ru>
2017-11-13 23:23:44 +0300

    shit commit

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -31,17 +31,40 @@ namespace Bot_Application1.Dialogs
             {
                 str = stream.ReadToEnd();
             }
-
-            using (StreamWriter sw = new StreamWriter(@"E:\output.html", false, System.Text.Encoding.Default))
-            {
-                sw.WriteLine(str);
-            }
             Regex reg = new Regex(@"http://www.povarenok.ru/recipes/show/[0-9]+/");
-            foreach (Match match in reg.Matches(str))
+            Regex article = new Regex(@"/[0-9]+/");
+            Regex reciept = new Regex(@"<td valign=""top"" style=""padding: 0px 0px 0px 6px;"">[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(]+</td>");
+            Regex NameRecieptReg = new Regex(@"<a href=""http://www.povarenok.ru/recipes/show/[0-9]+/"">[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(""]+</a>");
+            Match art;
+            //foreach (Match match in reg.Matches(str))
+            //{
+            //    art = article.Match(match.ToString());
+            //    break;
+            //}
+            art = article.Match(reg.Match(str).ToString());//парсим ответ с полученными рецептами, находим номер статьи, чтобы получить принт версию
+            site = "http://www.povarenok.ru/recipes/print" + art.ToString();
+            req = (HttpWebRequest)HttpWebRequest.Create(site);
+            resp = (HttpWebResponse)req.GetResponse();
+            using (StreamReader stream = new StreamReader(
+                 resp.GetResponseStream(), Encoding.GetEncoding(1251)))
             {
-                await context.PostAsync(match.ToString());
-                break;
+                str = stream.ReadToEnd();
             }
+            str = str.Replace("&quot;", "\"");
+            string NameReciept = NameRecieptReg.Match(str).ToString();
+            NameReciept = Regex.Match(NameReciept, @">[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(""]+<").ToString();
+            NameReciept = Regex.Match(NameReciept, @"[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(""]+").ToString();
+            await context.PostAsync(NameReciept);
+            foreach (Match match in reciept.Matches(str))//парсим ответ с принт странички на сам рецепт.
+                //сначала находим по нужному тегу, дальше откидываем тег, далее откидываем знаки > <
+                //ничего умнее не придумал, увы :/
+            {
+                string point = match.ToString();
+                point = Regex.Match(point, @">[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(]+<").ToString();
+                point = Regex.Match(point, @"[a-zA-Zа-яА-Я\s\-\.\,\!\?\)\(]+").ToString();
+                await context.PostAsync(point);
+            }
+            //await context.PostAsync(reciept.Match(str).ToString());
             context.Wait(MessageReceivedAsync);
         }
     }
