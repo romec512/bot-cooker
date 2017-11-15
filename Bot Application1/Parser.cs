18372916156e5e1bfb722123527dcc150e1372ce
romec512 <roma-asadov@mail.ru>
2017-11-15 21:04:27 +0300

    commit

--- Bot Application1/Parser.cs
+++ Bot Application1/Parser.cs
@@ -12,7 +12,7 @@ using System.Web;
 namespace Bot_Application1
 {
     public class Parser
-    {
+    { 
         private static string GetPage(string site, Activity message)
         {
 
@@ -29,13 +29,45 @@ namespace Bot_Application1
             return str;
         }
 
-        public static string ParseListReciepts(string reciept, Activity message)
+        private static string GetPage(string site)
+        {
+            string str;
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(site);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            using (StreamReader stream = new StreamReader(
+                 resp.GetResponseStream(), Encoding.GetEncoding(1251)))
+            {
+                str = stream.ReadToEnd();
+            }
+            return str;
+        }
+
+        public static string[,] ParseListReciepts(string reciept, Activity message)//метод для парсинга странички с 10 рецептами
         {
             string site = "http://www.povarenok.ru/recipes/search/?name=";
             Regex reg = new Regex(@"<a href=""http://www.povarenok.ru/recipes/show/[\d]+/"" title=""посмотреть рецепт [^>]+"">[^<]+</a>");
             Regex reg1 = new Regex(@">[^<]+<");
             Regex NameOfRec = new Regex(@"[^<]");
             string page = GetPage(site, message);
+            //....
+            string[,] result = new string[2,10];//массив с результатами парсинга, в 0 строке названия рецептов, в 1 строке ссылки на рецепт
+            return result;
+        }
+
+        public static string ParseReciept(string site)//метод для парсинга странички с рецептом, site - ссылка на нужную страничку
+        {
+           
+        }
+
+        public static string ParseNameReciept(string site)//метод для парсинка странички с рецептом(для нахождения названия рецепта)
+        {
+
+        }
+
+        public static string ParseIngredient(string site)//метод для парсинга странички с рецептом(для нахождения игредиентов)
+        {
+
         }
     }
 }
