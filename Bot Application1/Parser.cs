b4d6ec6a09ea6bc703bf722d883a2209fb94650c
romec512 <roma-asadov@mail.ru>
2017-11-15 20:33:36 +0300

    add parser, but is not finished

---
+++ Bot Application1/Parser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Bot_Application1
+{
+    public class Parser
+    {
+        private static string GetPage(string site, Activity message)
+        {
+
+            var str = HttpUtility.UrlEncode(message.Text, Encoding.GetEncoding(1251));
+            site = site + str;
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(site);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            using (StreamReader stream = new StreamReader(
+                 resp.GetResponseStream(), Encoding.GetEncoding(1251)))
+            {
+                str = stream.ReadToEnd();
+            }
+            return str;
+        }
+
+        public static string ParseListReciepts(string reciept, Activity message)
+        {
+            string site = "http://www.povarenok.ru/recipes/search/?name=";
+            Regex reg = new Regex(@"<a href=""http://www.povarenok.ru/recipes/show/[\d]+/"" title=""посмотреть рецепт [^>]+"">[^<]+</a>");
+            Regex reg1 = new Regex(@">[^<]+<");
+            Regex NameOfRec = new Regex(@"[^<]");
+            string page = GetPage(site, message);
+        }
+    }
+}
